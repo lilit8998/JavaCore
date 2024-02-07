fd2bf99d867c4b8c5f4d8844e1edf008fde35930
Lilit Poghosyan <poghosyanlilit544@gmail.com>
2024-02-08 00:17:18 +0400

    Added DynamicArray class and all methods of DynamicArray

---
+++ src/dynamicArray/DynamicArray.java
@@ -0,0 +1,105 @@
+package dynamicArray;
+
+public class DynamicArray {
+    //սա մեր հիմնական մասիվն է, որտեղ պահելու ենք ավելացվող էլեմենտները
+    private int[] array = new int[10];
+    //սա մեր մասիվի մեջ ավելացված էլեմենտների քանակն է
+    private int size = 0;
+
+    //ստուգել եթե մասիվի մեջ տեղ չկա-> կանչել extend()
+    //և ավելացնենք
+    public void add(int value) {
+        if (size == array.length){
+            extend();
+        }else {
+           array[size++] = value;
+        }
+
+    }
+
+    //1․ ստեղծել հին մասիվից 10 էլեմենտ ավելի մեծ մասիվ
+    //2․ քցել հին մասիվի էլեմենտները նորի մեջ
+    //3․ հին մասիվի հղումը կապենք նոր մասիվի հղման հետ։
+    private void extend() {
+        int[] newTemp = new int[array.length + 10];
+        for (int i =0; i < array.length; i++){
+            newTemp[i] = array[i];
+        }
+        array = newTemp;
+    }
+
+    //եթե տրված ինդեքսը մեր ունեցած մասիվի ինդեքսի սահմաններում է, վերադարձնել
+    // մասիվի index-երրորդ էլեմենտը։ Հակառակ դեպքում վերադարձնել -1։
+    public int getByIndex(int index) {
+        if (index < size && index >= 0){
+            return array[index];
+        }else {
+            return -1;
+        }
+    }
+
+    //տպել մասիվի ավելացված էլեմենտները
+    public void print() {
+        for (int i = 0; i < size; i++){
+            System.out.println(array[i] + " ");
+        }
+
+    }
+
+    //ջնջել մասիվի index-երորդ էլեմենտը
+//եթե չկա նման ինդեքսով էլեմենտ, տպել, որ չկա նման էլեմենտ
+    public void deleteByIndex(int index) {
+        if (index < 0 || index >= size){
+            System.out.println("Invalid index");
+        }else {
+            for (int i = index; i < size; i++){
+                array[i] = array[i+1];
+            }
+        }
+
+        size--;
+
+    }
+    //տրված value-ն դնելու ենք տրված index-ով էլեմենտի տեղը։
+//Հին արժեքը կկորի
+//եթե նման ինդեքսով էլեմենտ չկա, գրում ենք որ չկա։
+    public void set(int index, int value) {
+            if (index < 0 || index >= array.length){
+                System.out.println("Invalid index");
+            }
+            array[index] = value;
+    }
+//    ավելացնել տրված value-ն տրված ինդեքսում, իսկ էղած էլեմենտները մի հատ աջ տանել։
+//Եթե չկա նման ինդեքս, տպել որ չկա
+    public void add(int index, int value) {
+        if (index > 0 && index < array.length){
+            for (int i = array.length-1; i >= index; i--){
+                array[i] = array[i - 1];
+            }
+            array[index] = value;
+        }else {
+            System.out.println("Index doesn't exist");
+        }
+    }
+    //Վերադարձնել true եթե տրված value-ն կա մեր մասիվի մեջ, եթե ոչ false
+    public boolean exists(int value) {
+        for (int i = 0; i < array.length; i++){
+            if (array[i] == value){
+                return true;
+            }
+        }
+        return false;
+    }
+//    Վերադարձնել տրված value-ի ինդեքսը, եթե շատ կա տվյալ թվից, վերադարձնել առաջին ինդեքսը։
+//եթե չկա, -1
+    public int getIndexByValue(int value) {
+        for (int i = 0; i < array.length; i++){
+            if (array[i] == value){
+                System.out.println(i);
+                return  i;
+            }
+        }
+        return -1;
+    }
+
+}
